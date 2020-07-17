4006c67b64f99a51b81173c8baa6c0e50174ff73
张俊杰 <57305595@qq.com>
2020-07-17 16:27:43 +0800

    模拟按键

--- main.go
+++ main.go
@@ -6,25 +6,40 @@ import (
 	"fyne.io/fyne/app"
 	"fyne.io/fyne/theme"
 	"fyne.io/fyne/widget"
+	"github.com/PuerkitoBio/goquery"
 	"github.com/tebeka/selenium"
+	"github.com/tebeka/selenium/chrome"
 	url2 "net/url"
 	"os"
 	"regexp"
 	"strings"
 	"time"
-	"github.com/PuerkitoBio/goquery"
 )
 
 var button *widget.Button
 
 func main()  {
+
+
 	app := app.NewWithID("io.phpzjj.demo")
 	app.SetIcon(theme.FyneLogo())
 	w := app.NewWindow("东航周末随心飞监控软件")
 	w.Resize(fyne.NewSize(500,600))
 
-	url := widget.NewEntry()
-	url.SetPlaceHolder("URL")
+
+	//起点
+	starting := widget.NewEntry()
+	starting.SetPlaceHolder("start")
+
+	//终点
+	target := widget.NewEntry()
+	target.SetPlaceHolder("target")
+
+
+	//起飞时间
+	depDt := widget.NewEntry()
+	depDt.SetPlaceHolder("depDt (eg:2020-08-02)")
+
 
 	code := widget.NewEntry()
 	code.SetPlaceHolder("code")
@@ -41,12 +56,14 @@ func main()  {
 
 	button = widget.NewButton("start", func() {
 		info.SetText("Start ->  [" + code.Text + "]")
-		go start(url.Text, code.Text, app, info)
+		go start(starting.Text, target.Text, depDt.Text, code.Text, app, info)
 	})
 
 
 	w.SetContent(widget.NewVBox(
-		url,
+		starting,
+		target,
+		depDt,
 		hyperlink,
 		code,
 		button,
@@ -62,13 +79,57 @@ func main()  {
 	w.ShowAndRun()
 }
 
-func start( url string,  code string, app fyne.App, info *widget.Entry) {
+func start(starting string, target string, depDt string,code string, app fyne.App, info *widget.Entry) {
 
 	button.SetText("ing")
 	button.Disable()
-	// Start a Selenium WebDriver server instance (if one is not already
-	// running).
 
+	for true {
+
+
+		html := form(starting, target, depDt)
+
+		if html == "false" {
+			now := time.Now()
+			dateString := fmt.Sprintf("%d-%d-%d %d:%d:%d",now.Year(),now.Month(),now.Day(),now.Hour(),now.Minute(),now.Second())
+			info.SetText( dateString +"    ----->   no! \n" + info.Text)
+		}
+
+		doc,_ := goquery.NewDocumentFromReader(strings.NewReader(html))
+
+		doc.Find("#sylvanas_3 .flight").Each(func(i int, s *goquery.Selection) {
+
+			band := s.Find(".title").Text()
+			match, _ := regexp.MatchString("("+code+")", band)
+
+			if (match) {
+				number,_ :=  s.Find("*[name='lowest']").First().Attr("class")
+				match, _ := regexp.MatchString("(noPrice)", number)
+				fmt.Println(number)
+				if(match) {
+					now := time.Now()
+					dateString := fmt.Sprintf("%d-%d-%d %d:%d:%d",now.Year(),now.Month(),now.Day(),now.Hour(),now.Minute(),now.Second())
+					info.SetText( dateString +"    ----->   no \n" + info.Text)
+				} else {
+					app.SendNotification(&fyne.Notification{
+						Title: "随心飞提示:",
+						Content: "有余票",
+					})
+				}
+			}
+
+		})
+		time.Sleep(30*time.Second)
+
+	}
+
+}
+
+
+/*
+表单操作提取数据
+*/
+func form (starting string, target string,  depDt string) string{
 	pwd, _ := os.Getwd()
 	seleniumPath := pwd+"/chromedriver"
 	const (
@@ -89,47 +150,144 @@ func start( url string,  code string, app fyne.App, info *widget.Entry) {
 	// Connect to the WebDriver instance running locally.
 	caps := selenium.Capabilities{"browserName": "chrome"}
 
-		wd, err := selenium.NewRemote(caps, fmt.Sprintf("http://localhost:%d/wd/hub", port))
-		if err != nil {
-			panic(err)
-		}
-		defer wd.Quit()
 
-	for true {
-		if err := wd.Get(url); err != nil {
-			panic(err)
-		}
-		html, _ := wd.PageSource()
-
-		time.Sleep(30*time.Second)
-
-		doc,_ := goquery.NewDocumentFromReader(strings.NewReader(html))
-
-		doc.Find("#sylvanas_3 .flight").Each(func(i int, s *goquery.Selection) {
-
-			band := s.Find(".title").Text()
-			match, _ := regexp.MatchString("("+code+")", band)
-
-			if (match) {
-				number,_ :=  s.Find("*[name='lowest']").First().Attr("class")
-				match, _ := regexp.MatchString("(noPrice)", number)
-
-				if(match) {
-					now := time.Now()
-					dateString := fmt.Sprintf("%d-%d-%d %d:%d:%d",now.Year(),now.Month(),now.Day(),now.Hour(),now.Minute(),now.Second())
-					info.SetText( dateString +"    ----->   no \n" + info.Text)
-				} else {
-					app.SendNotification(&fyne.Notification{
-						Title: "随心飞提示:",
-						Content: "有余票",
-					})
-				}
-			}
-
-		})
-
-		time.Sleep(10*time.Minute)
-
+	chromeCaps := chrome.Capabilities{
+		Args: []string{
+			"--headless", // 设置Chrome无头模式
+			"--no-sandbox",
+			//"--blink-settings=imagesEnabled=false",
+			//"--disable-gpu",
+			"--disable-images",
+			//"--proxy-server=116.196.88.52:4444",
+			"--user-agent = Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0.2 Safari/604.4.7", // 模拟user-agent，防反爬
+		},
 	}
 
+	/*caps.AddProxy(selenium.Proxy{
+		Type:          selenium.Manual,
+		SOCKSVersion:     5,
+		SOCKS:         "116.196.88.52:4444",
+		NoProxy: []string{
+			"http://www.ceair.com/upload/2018/9/20190925daxingjichang.jpg",
+			"http://www.ceair.com/ad/sytytd/202003/W020200312633151698975.png",
+			"http://static-cdn.ceair.com/resource/images/public/map-mark-old.png?v=zh_CN_18459",
+		},
+	})
+*/
+	caps.AddChrome(chromeCaps)
+
+
+	wd, err := selenium.NewRemote(caps, fmt.Sprintf("http://localhost:%d/wd/hub", port))
+
+
+
+	defer  wd.Quit()
+
+
+	if err != nil {
+		panic(err)
+	}
+
+	wd.Get("http://lumtest.com/myip.json")
+	ip,_ := wd.PageSource()
+	fmt.Println(ip)
+
+	if err := wd.Get("http://www.ceair.com"); err != nil {
+		return "false"
+	}
+
+
+
+
+	time.Sleep(10*time.Second)
+	//关闭广告
+	gg,err := wd.FindElement(selenium.ByClassName, "pop_wrap_close")
+
+	if err != nil {
+		return "false"
+	}
+
+	gg.Click()
+
+	//输入起始地址
+	startElement,err := wd.FindElement(selenium.ByID, "label_ID_0")
+
+	if err != nil {
+		return "false"
+	}
+
+	startElement.Clear()
+	startElement.SendKeys(starting)
+
+	//点击下拉联想
+	time.Sleep(8*time.Second)
+
+	drag,err := wd.FindElement(selenium.ByXPATH, "//body/div/div/div/div/div/div/div/ul/li[1]")
+	if err != nil {
+		return "false"
+	}
+	drag.Click()
+
+
+	//输入抵达地址
+	time.Sleep(1*time.Second)
+	targetElement,err := wd.FindElement(selenium.ByID, "label_ID_1")
+	if err != nil {
+		return "false"
+	}
+	targetElement.Clear()
+	targetElement.SendKeys(target)
+
+
+	//点击下拉联想
+	time.Sleep(8*time.Second)
+	drag,err = wd.FindElement(selenium.ByXPATH, "//div//div//div//div//div//div//div[1]//ul[1]")
+	if err != nil {
+		return "false"
+	}
+	drag.Click()
+
+
+	//输入抵达时间
+
+	depDtElement,err := wd.FindElement(selenium.ByID, "depDt")
+	if err != nil {
+		return "false"
+	}
+	depDtElement.Click()
+	depDtElement.Clear()
+	wd.ExecuteScript("depDt.value='"+depDt+"'", nil)
+
+
+	depDtCloseElement,err := wd.FindElement(selenium.ByXPATH, "//html//body")
+	if err != nil {
+		return "false"
+	}
+	depDtCloseElement.Click()
+
+	//点击提交
+	btn,err := wd.FindElement(selenium.ByID, "btn_flight_search")
+	if err != nil {
+		return "false"
+	}
+	btn.Click()
+
+
+
+	//切换窗口
+	WindowHandles,_ := wd.WindowHandles()
+	if (len(WindowHandles) != 2 ) {
+		return  "false"
+	}
+	wd.SwitchWindow(WindowHandles[1])
+
+
+	//东航接口慢等它先加载下
+	time.Sleep(20*time.Second)
+	html,_ := wd.PageSource()
+
+	return html
+
 }
+
+
